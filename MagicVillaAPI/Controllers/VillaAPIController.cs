d3dfce138997a62c4dacae2479cc80e3b24b0191
javid <52424037+javidhasanzade@users.noreply.github.com>
2022-10-13 12:25:16 +0200

    v1

---
+++ MagicVillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,23 @@
+﻿using MagicVillaAPI.Data;
+using MagicVillaAPI.Models;
+using MagicVillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVillaAPI.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class VillaAPIController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<VillaDTO> GetVillas()
+    {
+        return VillaStore.Villas;
+    }
+
+    [HttpGet("id")]
+    public VillaDTO GetVilla(int id)
+    {
+        return VillaStore.Villas.FirstOrDefault(x => x.Id == id) ?? throw new NullReferenceException();
+    }
+}
---
+++ MagicVillaAPI/Data/VillaStore.cs
@@ -0,0 +1,12 @@
+﻿using MagicVillaAPI.Models.Dto;
+
+namespace MagicVillaAPI.Data;
+
+public static class VillaStore
+{
+    public static List<VillaDTO> Villas = new List<VillaDTO>()
+    {
+        new VillaDTO { Id = 1, Name = "Villa1" },
+        new VillaDTO { Id = 2, Name = "Villa2" }
+    };
+}
